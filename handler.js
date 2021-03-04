e8e453fc0b98b83ce451c13ebfa21b61606aae0d
sebknight <sebastianknightmaddox@gmail.com>
2021-03-04 18:17:57 +1300

    implement functions

---
+++ handler.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const thoughtsCreate = require('./thoughts-create.js');
+const thoughtsReadAll = require('./thoughts-read-all.js');
+const thoughtsReadOne = require('./thoughts-read-one.js');
+const thoughtsUpdate = require('./thoughts-update.js');
+const thoughtsDelete = require('./thoughts-delete.js');
+
+module.exports.create = (event, context, callback) => {
+  thoughtsCreate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readAll = (event, context, callback) => {
+  thoughtsReadAll(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.readOne = (event, context, callback) => {
+  thoughtsReadOne(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.update = (event, context, callback) => {
+  thoughtsUpdate(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+    context.succeed(response);
+  });
+};
+
+module.exports.delete = (event, context, callback) => {
+  thoughtsDelete(event, (error, result) => {
+    const response = {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin" : "*"
+      },
+      body: JSON.stringify(result),
+    };
+
+
+
+    context.succeed(response);
+  });
+};
